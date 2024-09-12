8d84d3d13192aada5ad221fa00e4474aaa396b38
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-09-12 07:56:53 +0530

    Write encryption and decryption methods inside 'CryptoService' class

--- service/Core/Interfaces/Utility/ICryptoService.cs
+++ service/Core/Interfaces/Utility/ICryptoService.cs
@@ -12,5 +12,21 @@
         /// <param name="privateKey">The private key used for decryption.</param>
         /// <returns>The decrypted password as a string.</returns>
         string DecryptPassword(string encryptedPassword, string privateKey);
+
+        /// <summary>
+        /// Encrypts a plaintext string using AES encryption.
+        /// </summary>
+        /// <param name="plainText">The plaintext string to be encrypted.</param>
+        /// <param name="key">The encryption key. If null, a default key will be used.</param>
+        /// <returns>The encrypted ciphertext as a string.</returns>
+        Task<string> AesEncryptionAsync(string plainText, byte[]? key = null);
+
+        /// <summary>
+        /// Decrypts an AES-encrypted ciphertext string.
+        /// </summary>
+        /// <param name="cipherText">The ciphertext string to be decrypted.</param>
+        /// <param name="key">The decryption key. If null, a default key will be used.</param>
+        /// <returns>The decrypted plaintext as a string.</returns>
+        Task<string> AesDecryptionAsync(string cipherText, byte[]? key = null);
     }
 }
